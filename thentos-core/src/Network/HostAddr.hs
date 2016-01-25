a93074a082b60ab0e76f7bb1052710a092252182
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2016-01-25 14:30:57 +0100

    [Issue #455] Add support for IPv6 addresses in allow_ips configuration
    * Instead of `inet_addr`, use `getAddrInfo` which supports IPv6
    * A new module `Network.HostAddr` including:
        * `HostAddr` a type wrapping the different kind of addresses
        * `getHostAddr` a wrapper over `getAddrInfo` returning a `HostAddr`
        * `hostAddr` extract `HostAddr` from a `SockAddr`

---
+++ thentos-core/src/Network/HostAddr.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Network.HostAddr
+where
+
+import Control.Monad.Except (throwError)
+import Data.Int (Int32)
+import Network.Socket
+          (SockAddr(SockAddrInet, SockAddrInet6, SockAddrUnix, SockAddrCan),
+           HostAddress, HostAddress6, SocketType(Stream), AddrInfoFlag(AI_NUMERICHOST),
+           addrAddress, getAddrInfo, addrFlags, addrSocketType, defaultHints)
+
+data HostAddr
+  = HostAddress  HostAddress
+  | HostAddress6 HostAddress6
+  | UnixAddress  String
+  | CanAddress   Int32
+  deriving (Eq, Read, Show)
+
+hostAddr :: SockAddr -> HostAddr
+hostAddr = \case
+  SockAddrInet _ ip -> HostAddress ip
+  SockAddrInet6 _ _ ip _ -> HostAddress6 ip
+  SockAddrUnix fp -> UnixAddress fp
+  SockAddrCan i -> CanAddress i
+
+-- TODO add our IP format validation
+getHostAddr :: String -> IO HostAddr
+getHostAddr name = hostAddr . addrAddress . single <$> getAddrInfo (Just hints) (Just name) Nothing
+  where
+    hints = defaultHints { addrFlags = [AI_NUMERICHOST], addrSocketType = Stream }
+
+    single [x] = x
+    single []  = error "Impossible error: TODO not enough"
+    single _   = error "Impossible error: too many results from getAddrInfo should have been ruled out by our `hints`"
